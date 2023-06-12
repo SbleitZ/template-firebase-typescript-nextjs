a6240300d18663bf7e1238d3cf913e9287e477a2
Sbleit <zptrack24@gmail.com>
2023-06-12 11:05:21 -0400

    add error message in reset password

--- src/app/resetpassword/page.tsx
+++ src/app/resetpassword/page.tsx
@@ -1,17 +1,31 @@
 "use client";
-import{ ChangeEvent, useState } from "react";
+import{FormEvent,ChangeEvent, useState } from "react";
 import { useAuth } from "../Hooks/AuthContext";
+import { errorsFirebase } from "../../../firebase/utils";
 export default function ResetPassword(){
   const [email,setEmail] = useState('');
+  const [errorSubmit, setErrorSubmit ] = useState('');
   const { resetPassword } = useAuth();
-  async function onSubmit(){
-    await resetPassword(email)
+  async function onSubmit(e:FormEvent<HTMLFormElement>){
+    e.preventDefault()
+    try {
+      await resetPassword(email)
+    } catch (error:any) {
+      try {
+        setErrorSubmit(errorsFirebase[error.code])
+      } catch (error:any) {
+        setErrorSubmit(error.code)
+      }
+    }
+    
   }
   return (
     <>
       <form onSubmit={onSubmit} className="flex flex-col items-center justify-center gap-2">
+        <div className="text-red-600 text-sm">{errorSubmit}</div>
+
         <h2>Ingresa el correo para recuperarlo</h2>
-        <input onChange={(e:ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} type="email" name="" placeholder="Tu email" />
+        <input className="placeholder:text-slate-400 rounded-md p-4 border-2 border-solid border-slate-400 outline-none text-black" onChange={(e:ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} type="email" name="" placeholder="Tu email" />
         <button className="bg-white text-black rounded-lg p-2">Recuperar</button>
       </form>
     </>
